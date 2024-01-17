b5992a4afbc7b0eae739c6370b631358f4db0c32
jchen <j.chen5@student.han.nl>
2024-01-17 17:20:40 +0100

    feat: add Parola layout

---
+++ src/main/java/ParolaController.java
@@ -0,0 +1,30 @@
+public class ParolaController {
+
+    public static ParolaController getInstance() {
+        return null;
+    }
+
+    public void startQuiz(String username) {
+
+    }
+
+    public String nextQuestion(String username) {
+        return "";
+    }
+
+    public void processAnswer(String username, String answer) {
+
+    }
+
+    public boolean quizFinished(String username) {
+        return false;
+    }
+
+    public String getLettersForRightAnswers(String username) {
+        return null;
+    }
+
+    public int calculateScore(String username, String word) {
+        return 0;
+    }
+}
